e6846b90505f1f589bb8c2fa374971e2fb7bb4fe
Jacob Nordstrom <jacobnordstrom9@gmail.com>
2021-07-22 13:02:10 -0400

    cors to server

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
 const helmet = require("helmet");
+const cors = require("cors");
 app.use(helmet());
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers':
+        'Origin, X-Requested-With, Content-Type, Accept',
+};
 const port = process.env.PORT || 3001;
 
 
 app.use(express.urlencoded({ extended: false })); //scrape email and pass from request header
 app.use(express.json());
-
+app.use(cors(corsOptions))
 app.get('/', (req, res) => {
     res.send('home page')
 })
